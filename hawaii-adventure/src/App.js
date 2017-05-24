7f22e68d7ffbe0a0d2823332cb7312fa2276dae9
Ivy Patton <lilaivy@gmail.com>
2017-05-24 07:45:10 -0700

    still working on it

--- hawaii-adventure/src/App.js
+++ hawaii-adventure/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import UserSignin from './UserSignin';
-import Image from './images/map.jpg';
-import Pina from './images/pina.jpg';
-import Luau from './images/luau.jpg'
+import Transport from './Transport';
+import pina from './images/pina.jpg';
+import luau from './images/luau.jpg'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      CurrentScene: Transport,
       name: '',
       message: '',
-      // nextScene:nextSence
-      image: '',
+     
       text: ''
 
     }
@@ -37,7 +37,7 @@ class App extends Component {
   }
 
   render() {
-    const { name, message, image } = this.state;
+    const { name, message } = this.state;
 
 
     return (
@@ -46,36 +46,15 @@ class App extends Component {
           name={name}
           changeName={this.changeName}
         />
-        {/* <SceneOne/>*/}
-
-        <div className="adventureMessage"></div>
-        <h2>Good Morning {name}, you have been transported to Hawaii. Congratulations! </h2>
-        <img src={Image} className="welcomeImage" />
-        <h2>Send A message to all your rained out PDX homies: {this.state.message}</h2>
-        <form className="message-form"
-          onSubmit={event => {
-            event.preventDefault();
-            this.changeMessage(event.target.elements.message.value);
-          }}
-        >
-          <input style={{
-            fontSize: '15px',
-            borderRadius: '5px',
-            backgoundColor: 'while'
-          }}
-            name='message'
-            type='text'
-            placeholder='Message To Homies:' />
-          <input style={{
-            fontSize: '15px',
-            borderRadius: '5px',
-            backgroundColor: 'white'
-          }}
-            type='submit'
-            name='submit'
-          />
-        </form>
+        <Transport
+          changeMessage={this.changeMessage}
+          message={message}
+          name={name}
+        />
         <button className="move-on" onClick={() => this.changeScene('nextScene')}>CheckOut What Happens Next!</button>
+
+
+
 
 
         <div className="adventureMessage">
@@ -83,15 +62,15 @@ class App extends Component {
 
           <h2>But grab a Pina Colada first, duh!</h2>
 
-          <img src={Pina} className="pinaImage" onClick={this.changeImage('')} />
+          <img src={pina} className="pinaImage" onClick={this.changeImage} />
           <form className="message-form">
 
           </form>
           <div className="Luau">
-             <h2>Oh no {name}! You've encountered a touristy Luau that is keeping you from assessing he beach! </h2>
-            <img src={Luau} className="luau"/>
+            <h2>Oh no {name}! You've encountered a touristy Luau that is keeping you from assessing he beach! </h2>
+            <img src={luau} className="luau" />
           </div>
-        
+
         </div>
       </div>
 
